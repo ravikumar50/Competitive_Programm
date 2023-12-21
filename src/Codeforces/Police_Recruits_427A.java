b5f0dfd583a586da4e41c50f5b612a2b64014d17
ravikumar50 <ravikumarsingh5256@gmail.com>
2023-12-21 12:39:54 +0530

    Police Recruits Problem Added

---
+++ src/Codeforces/Police_Recruits_427A.java
@@ -0,0 +1,26 @@
+package Codeforces;
+import java.util.*;
+public class Police_Recruits_427A {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int police = 0;
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            if (a == -1) {
+                if (police > 0) {
+                    police--;
+                } else {
+                    ans++;
+                }
+            } else {
+                police += a;
+
+            }
+        }
+        System.out.println(ans);
+    }
+}
