9a6b58ddaf320eb786957c65bf4fdebf93452c17
ravikumar50 <ravikumarsingh5256@gmail.com>
2023-12-22 00:03:29 +0530

    Gravity Flip Problem Added

---
+++ src/Codeforces/Rating_900/Gravity_Flip_405A.java
@@ -0,0 +1,18 @@
+package Codeforces.Rating_900;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Gravity_Flip_405A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0; i<n; i++) arr[i] = sc.nextInt();
+        Arrays.sort(arr);
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
