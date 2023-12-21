3f7840107cc2b0800987da0ed2b9cd760a2dd95d
ravikumar50 <ravikumarsingh5256@gmail.com>
2023-12-21 12:37:49 +0530

    Bear and Big Brother Problem

---
+++ src/Codeforces/Bear_And_Big_Brother_791A.java
@@ -0,0 +1,19 @@
+package Codeforces;
+
+import java.util.*;
+public class Bear_And_Big_Brother_791A {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int ans = 0;
+        while(a<=b){
+            a = 3*a;
+            b = 2*b;
+            ans++;
+        }
+        System.out.println(ans);
+    }
+}
