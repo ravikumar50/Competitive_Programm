0c583ec7609f0231c2bf1577049f2f2b9d51a1c4
ravikumar50 <ravikumarsingh5256@gmail.com>
2023-12-21 12:33:15 +0530

    File name changed

--- src/Codeforces/Way_To_Long_71A.java
+++ src/Codeforces/Way_To_Long_71A.java
@@ -1,7 +1,7 @@
 package Codeforces;
 
 import java.util.*;
-public class Way_To_Long {
+public class Way_To_Long_71A {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
