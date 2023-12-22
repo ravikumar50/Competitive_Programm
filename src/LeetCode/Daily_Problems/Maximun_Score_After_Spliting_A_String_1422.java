86e679f71d5c43834100c46546763d01071efb62
ravikumar50 <ravikumarsingh5256@gmail.com>
2023-12-22 10:42:18 +0530

    Maximum Score After spliting a String Daily Problem of Leetcode Added

---
+++ src/LeetCode/Daily_Problems/Maximun_Score_After_Spliting_A_String_1422.java
@@ -0,0 +1,38 @@
+package LeetCode.Daily_Problems;
+
+public class Maximun_Score_After_Spliting_A_String_1422 {
+
+    public static int maxScore(String s) {
+        int n = s.length();
+        int ones = 0;
+        int zero = 0;
+
+        for(int i=0; i<n; i++){
+            if(s.charAt(i)=='0') zero++;
+            else ones++;
+        }
+
+        int left = 0;
+        int right = 0;
+        int ans = 0;
+        for(int i=0; i<n-1; i++){
+            if(s.charAt(i)=='0'){
+                left++;
+                right = ones;
+                ans = Math.max(ans,left+right);
+            }else{
+                ones--;
+                right=ones;
+                ans = Math.max(ans,left+right);
+            }
+        }
+        return ans;
+
+    }
+
+    public static void main(String[] args) {
+        String s = "011101";
+        int ans = maxScore(s);
+        System.out.println(ans);
+    }
+}
