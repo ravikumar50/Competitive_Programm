0e29e200e080ab2401ca4a179ea6def8bcf610a6
ravikumar50 <ravikumarsingh5256@gmail.com>
2023-12-22 10:38:04 +0530

    Question Statement Added For Maximum Meeting Problem

--- src/GFG/Daily_Problems/Maximum_Meetings_In_A_Room.java
+++ src/GFG/Daily_Problems/Maximum_Meetings_In_A_Room.java
@@ -3,6 +3,17 @@ package GFG.Daily_Problems;
 
 import java.util.*;
 
+/*
+ There is one meeting room in a firm. There are N meetings in the form of (S[i], F[i]) where S[i]
+ is the start time of meeting i and F[i] is the finish time of meeting i. The task is to find the
+ maximum number of meetings that can be accommodated in the meeting room. You can accommodate a
+ meeting if the start time of the meeting is strictly greater than the finish time of the previous
+ meeting. Print all meeting numbers.
+
+Note: If two meetings can be chosen for the same slot then choose meeting with smaller index
+ in the given array.
+ */
+
 class Meeting{
     int st;
     int end;
