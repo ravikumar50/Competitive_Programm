6faf68f1c7d08e452ecc6ef68530230c501c177f
ravikumar50 <ravikumarsingh5256@gmail.com>
2023-12-28 01:05:36 +0530

    Squares Problem Added

---
+++ src/Codeforces/Rating_900/Squares_1A.java
@@ -0,0 +1,24 @@
+package Codeforces.Rating_900;
+
+import java.util.Scanner;
+
+public class Squares_1A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextInt();
+        long m = sc.nextInt();
+        long a = sc.nextInt();
+
+        if(n%a==0 && m%a==0){
+            System.out.println((n*m)/(a*a));
+        }else if(n%a==0){
+            System.out.println((n/a)*((m/a)+1));
+        }else if(m%a==0){
+            System.out.println((m/a)*((n/a)+1));
+        }else{
+            long x = n/a;
+            long y = m/a;
+            System.out.println((x*y)+(x+y+1));
+        }
+    }
+}
