71a591617c16c9f70697e6f5e252e893c6fb6859
ravikumar50 <ravikumarsingh5256@gmail.com>
2023-12-28 01:40:02 +0530

    Young Physicist Problem Added

---
+++ src/Codeforces/Rating_1000/Young_Physicist_69A.java
@@ -0,0 +1,24 @@
+package Codeforces.Rating_1000;
+
+import java.util.Scanner;
+
+public class Young_Physicist_69A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = 0;
+        int y = 0;
+        int z = 0;
+
+        for(int i=0; i<n; i++){
+            x+=sc.nextInt();
+            y+=sc.nextInt();
+            z+=sc.nextInt();
+        }
+        if(x==0 && y==0 && z==0){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+    }
+}
