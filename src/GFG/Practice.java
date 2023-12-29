27f53f9d7284a2b49e7b53b548b31cccb3c58114
ravikumar50 <ravikumarsingh5256@gmail.com>
2023-12-29 11:57:35 +0530

    GFG Daily Problem Added

--- src/GFG/Practice.java
+++ src/GFG/Practice.java
@@ -10,36 +10,8 @@ import java.util.*;
  */
 public class Practice {
 
-    static int kSubstrConcat(int n, String s, int k) {
-        // Your Code Here
-        if(n==k) return 1;
-        if(n%k!=0) return 0;
 
-        HashMap<String,Integer> hp = new HashMap<>();
-
-        for(int i=0; i<n; i=i+k){
-            String str = s.substring(i,i+k);
-            hp.put(str,hp.getOrDefault(str,0)+1);
-        }
-
-        if(hp.size()==1) return 1;
-        if(hp.size()!=2) return 0;
-
-        int size = 0;
-        for(var a : hp.values()){
-            size = a;
-            break;
-        }
-
-        if(size==(n/k-1) || size==1) return 1;
-        else return 0;
-
-    }
     public static void main(String[] args) {
-        int n = 24;
-        int k = 6;
-        String s = "zedsfbkxgxpzzedsfbzedsfb";
-        int ans = kSubstrConcat(n,s,k);
-        System.out.println(ans);
+
     }
 }
