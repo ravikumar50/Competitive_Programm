5e24cf17c76e55c0abca9bb4b74d85ae36432a24
ravikumar50 <ravikumarsingh5256@gmail.com>
2023-12-29 15:40:05 +0530

    1st Question Added

---
+++ src/Codeforces/Contest/Codeforces_Round_918_DIV_4/Odd_One_Out_A.java
@@ -0,0 +1,21 @@
+package Codeforces.Contest.Codeforces_Round_918_DIV_4;
+
+import java.util.Scanner;
+
+public class Odd_One_Out_A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while(t>0){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+
+            if(a==b) System.out.println(c);
+            else if(a==c) System.out.println(b);
+            else System.out.println(a);
+            t--;
+        }
+    }
+}
