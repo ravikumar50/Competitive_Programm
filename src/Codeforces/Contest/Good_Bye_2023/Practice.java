63b4f8bb3ffd02ac2024865827817557630bf174
ravikumar50 <ravikumarsingh5256@gmail.com>
2023-12-31 18:09:52 +0530

    Testing to add file without Internet

--- src/Codeforces/Contest/Good_Bye_2023/Practice.java
+++ src/Codeforces/Contest/Good_Bye_2023/Practice.java
@@ -1,7 +1,29 @@
 package Codeforces.Contest.Good_Bye_2023;
-
-import java.util.Scanner;
-
 public class Practice {
+    public static int longestSpecialSubstring(String s) {
+        int maxLength = -1;
+        int currentLength = 1;
 
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                currentLength++;
+            } else {
+                if (currentLength >= 3) {
+                    maxLength = Math.max(maxLength, currentLength);
+                }
+                currentLength = 1;
+            }
+        }
+
+        // Check the last substring
+        if (currentLength >= 3) {
+            maxLength = Math.max(maxLength, currentLength);
+        }
+
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
