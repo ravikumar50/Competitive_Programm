f5502b2f0b23aa1df991edf445850b308c184b0c
ravikumar50 <ravikumarsingh5256@gmail.com>
2023-12-31 23:38:31 +0530

    Count the number of Pairs Problem Added

--- src/Codeforces/Rating_1000/Count_the_Number_of_Pairs_1800B.java
+++ src/Codeforces/Rating_1000/Count_the_Number_of_Pairs_1800B.java
@@ -15,37 +15,21 @@ public class Count_the_Number_of_Pairs_1800B {
             int arr2[] = new int[26];
 
             for(var ch : s.toCharArray()){
-                if(ch>='a' && ch<'z') arr1[ch-'a']++;
+                if(ch>='a' && ch<='z') arr1[ch-'a']++;
                 else arr2[ch-'A']++;
             }
             int ans = 0;
             for(int i=0; i<26; i++){
                 int x = arr1[i];
                 int y = arr2[i];
-                if(x>y){
-                    ans = ans+y;
-                    int diff = x-y;
-                    int a = diff/2;
-                    if(a<=k){
-                        ans = ans+a;
-                        k=k-a;
-                    }else{
-                        ans = ans+k;
-                        break;
-                    }
-                }else if(x<y){
-                    ans = ans+x;
-                    int diff = y-x;
-                    int a = diff/2;
-                    if(a<=k){
-                        ans = ans+a;
-                        k=k-a;
-                    }else{
-                        ans = ans+k;
-                        k=0;
-                    }
+                ans = ans + Math.min(x,y);
+                int diff = Math.abs(x-y);
+                if(diff/2<=k){
+                    ans = ans+(diff/2);
+                    k = k-(diff/2);
                 }else{
-                    ans = ans+x;
+                    ans = ans + k;
+                    k=0;
                 }
             }
             System.out.println(ans);
