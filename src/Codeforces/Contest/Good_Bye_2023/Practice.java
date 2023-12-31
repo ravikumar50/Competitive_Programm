854326bac002bfb7018c6b6a369045af63d2eb58
ravikumar50 <ravikumarsingh5256@gmail.com>
2023-12-31 19:30:55 +0530

    Second Problem Added

--- src/Codeforces/Contest/Good_Bye_2023/Practice.java
+++
@@ -1,6 +0,0 @@
-package Codeforces.Contest.Good_Bye_2023;
-public class Practice {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/Codeforces/Contest/Good_Bye_2023/Two_Divisor.java
@@ -0,0 +1,28 @@
+package Codeforces.Contest.Good_Bye_2023;
+
+import java.util.Scanner;
+
+public class Two_Divisor {
+    static long gcd(long a, long b){
+        if(b==0) return a;
+        else return gcd(b,a%b);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while(t>0){
+            long a = sc.nextInt();
+            long b = sc.nextInt();
+
+            if(b%a==0){
+                System.out.println((b/a)*b);
+            }else{
+                long hcf = gcd(a,b);
+                long ans = (a*b)/hcf;
+                System.out.println(ans);
+            }
+            t--;
+        }
+    }
+}
