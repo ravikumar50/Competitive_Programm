9add3b17e01a1b548eb2455e8bef98fb82b496b7
ravikumar50 <ravikumarsingh5256@gmail.com>
2024-01-11 12:52:38 +0530

    1st Problem Added

---
+++ src/CodeChef/Contest/Starter_116/Minimum_Wage.java
@@ -0,0 +1,21 @@
+package CodeChef.Contest.Starter_116;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Minimum_Wage
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        if(x>11){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+
+    }
+}
+
