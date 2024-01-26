eb3e90c1010d2e18117ea1108328b9ab0450e72b
ravikumar50 <ravikumarsingh5256@gmail.com>
2024-01-26 17:08:55 +0530

    First Problem Added

---
+++ src/CodeChef/Contest/Starter_118/International_Education_Day.java
@@ -0,0 +1,12 @@
+package CodeChef.Contest.Starter_118;
+import java.util.*;
+public class International_Education_Day {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.println(Math.max(a*c,b*c));
+
+    }
+}
