bc2d30704f0d1e4d2c5959ca13747d67dd9128c5
ravikumar50 <ravikumarsingh5256@gmail.com>
2024-01-27 03:05:49 +0530

    Nothing important added

--- src/CodeChef/Contest/Practice.java
+++ src/CodeChef/Contest/Practice.java
@@ -4,5 +4,6 @@ public class Practice {
     public static void main(String[] args) {
         StringBuilder s = new StringBuilder("dsfk");
         StringBuilder ans = new StringBuilder(s.substring(1));
+
     }
 }
