5725489bab1f04c16083c389f268d9d51c79a05b
ravikumar50 <ravikumarsingh5256@gmail.com>
2024-01-27 20:00:45 +0530

    Nothing important

--- src/CodeChef/Contest/Practice.java
+++ src/CodeChef/Contest/Practice.java
@@ -1,9 +1,8 @@
 package CodeChef.Contest;
 
 public class Practice {
-    public static void main(String[] args) {
-        StringBuilder s = new StringBuilder("dsfk");
-        StringBuilder ans = new StringBuilder(s.substring(1));
 
+
+    public static void main(String[] args) {
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
     }
 }
