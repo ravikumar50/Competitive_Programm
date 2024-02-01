93c5894bf8fcf59852277f9d5ffaa95c848eb113
ravikumar50 <ravikumarsingh5256@gmail.com>
2024-02-02 00:18:14 +0530

    Third Problem Added

--- src/CodeChef/Contest/Starter_119/Cookie_Day.java
+++ src/CodeChef/Contest/Starter_119/Cookie_Day.java
@@ -1,29 +1,27 @@
-package CodeChef.Contest;
+package CodeChef.Contest.Starter_119;
 
-import java.util.*;
-import java.lang.*;
-import java.io.*;
+import java.util.Scanner;
 
-public class Cookie_Day{
-    public static void main (String[] args) throws java.lang.Exception{
+class Cookie_Day {
+    public static void main(String[] args) throws Exception {
         // your code goes here
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
 
-        while(t>0){
+        while (t > 0) {
             int n = sc.nextInt();
             int k = sc.nextInt();
             int arr[] = new int[n];
             int ans = Integer.MAX_VALUE;
-            for(int i=0; i<n; i++){
+            for (int i = 0; i < n; i++) {
                 int a = sc.nextInt();
-                if(a>=k){
-                    ans = Math.min(ans,a%k);
+                if (a >= k) {
+                    ans = Math.min(ans, a % k);
                 }
             }
-            if(ans==Integer.MAX_VALUE){
+            if (ans == Integer.MAX_VALUE) {
                 System.out.println(-1);
-            }else{
+            } else {
                 System.out.println(ans);
             }
 
@@ -32,3 +30,4 @@ public class Cookie_Day{
 
     }
 }
+
