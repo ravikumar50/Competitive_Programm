52ec99050e6779a49e626204dff9039bbd5b0040
ravikumar50 <ravikumarsingh5256@gmail.com>
2024-02-02 00:12:52 +0530

    First Problem Added

---
+++ src/CodeChef/Contest/Starter_119/Advitiya.java
@@ -0,0 +1,18 @@
+package CodeChef.Contest.Starter_119;
+import java.util.*;
+import java.lang.*;
+
+public class Advitiya {
+    public static void main (String[] args) throws java.lang.Exception {
+        // your code goes here
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if(n==16 || n==17 || n==18){
+            System.out.println("ADVITIYA");
+        }else{
+            System.out.println("WAITING FOR ADVITIYA");
+        }
+
+    }
+}
--- src/LeetCode/Practice.java
+++ src/LeetCode/Practice.java
@@ -1,13 +1,53 @@
 package LeetCode;
 
-public class Practice {
-    public static void main(String[] args) {
-        int a = 2;
-        int b = 5;
-        b = a+b;
-        a = b-a;
-        b = b-a;
+import java.util.*;
 
-        System.out.println(a+" "+b);
+public class Practice {
+
+    public static HashMap primeFactors(int n)
+    {
+        // Print the number of 2s that divide n
+        HashMap<Integer,Integer> hp = new HashMap<>();
+        while (n%2==0)
+        {
+            hp.put(2,hp.getOrDefault(2,0)+1);
+            n /= 2;
+        }
+
+        // n must be odd at this point.  So we can
+        // skip one element (Note i = i +2)
+        for (int i = 3; i <= Math.sqrt(n); i+= 2)
+        {
+            // While i divides n, print i and divide n
+            while (n%i == 0)
+            {
+                hp.put(i,hp.getOrDefault(i,0)+1);
+                n /= i;
+            }
+        }
+
+        // This condition is to handle the case when
+        // n is a prime number greater than 2
+        if (n > 2) hp.put(n,hp.getOrDefault(n,0)+1);
+        return hp;
     }
+
+    static int helper(int x, int p){
+        HashMap<Integer,Integer> hp = primeFactors(x);
+        int count = 0;
+        for(var a : hp.values()){
+            if(a!=1){
+                if(a%p==0) count++;
+            }
+        }
+        return (int)Math.pow(2,count);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = "df";
+
+    }
+
 }
