e46616ac7b39cc2196316971c8cf615da2b35b5f
ravikumar50 <ravikumarsingh5256@gmail.com>
2024-02-09 00:25:16 +0530

    First Problem Added

---
+++ src/CodeChef/Contest/Starter_120/Valentines_Contest.java
@@ -0,0 +1,20 @@
+package CodeChef.Contest.Starter_120;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Valentines_Contest
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n==121){
+            System.out.println("Likely");
+        }else{
+            System.out.println("UnLikely");
+        }
+
+    }
+}
