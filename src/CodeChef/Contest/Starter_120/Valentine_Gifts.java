9cdeea5b7eb16d9b35a2975f88436d85f15ce030
ravikumar50 <ravikumarsingh5256@gmail.com>
2024-02-09 01:01:05 +0530

    Second Problem Added

---
+++ src/CodeChef/Contest/Starter_120/Valentine_Gifts.java
@@ -0,0 +1,28 @@
+package CodeChef.Contest.Starter_120;
+import java.lang.reflect.Array;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Valentine_Gifts
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t>0){
+            int n = sc.nextInt();
+            if(n>=127){
+                System.out.println("YES");
+            }else{
+                System.out.println("NO");
+            }
+            t--;
+        }
+
+
+
+    }
+}
+
