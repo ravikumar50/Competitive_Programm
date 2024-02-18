5c1115e7e61d51798f2b6a55ef122ff395fd40f8
ravikumar50 <ravikumarsingh5256@gmail.com>
2024-02-18 14:37:31 +0530

    File added

--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,60 @@
-import java.util.*;
-
-import java.util.Random;
-
-// Main class to demonstrate Stop-and-Wait protocol
 public class Main {
-    static ArrayList<String> arr = new ArrayList<>();
-    static boolean reciever(String s){
-        int a = (int)(Math.random()*2);
-        if(a==1){
-            arr.add(s);
-            return true;
-        }else{
-            return false;
-        }
-    }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of data to be sent");
-        int n = sc.nextInt();
 
-        int idx = 0;
+    // Helper function to compute the KMP prefix array
+    static int[] computePrefixArray(String pattern) {
+        int n = pattern.length();
+        int[] prefixArray = new int[n];
+        int j = 0;
 
-        while(idx<n){
-            System.out.println("Enter Frame "+(idx+1));
-            String s = sc.next();
-            System.out.println("Sending Frame "+(idx+1));
-            boolean flag = reciever(s);
-            if(flag==true){
-                System.out.println("Recieved the acknowledgement for Frame "+(idx+1));
-                System.out.println("Frame "+(idx+1)+" sent Successfully");
-                System.out.println();
-                idx++;
-            }else{
-                System.out.println("Did not recieved the acknowledgement of Frame "+(idx+1));
-                System.out.println("Resending the Frame "+(idx+1));
-                System.out.println();
-                flag = reciever(s);
-                while (flag!=true){
-                    System.out.println("Did not recieved the acknowledgement of Frame "+(idx+1));
-                    System.out.println("Resending the Frame "+(idx+1));
-                    System.out.println();
-                    flag = reciever(s);
-                }
-                System.out.println("Recieved the acknowledgement for Frame "+(idx+1));
-                System.out.println("Frame "+(idx+1)+" sent Successfully");
-                idx++;
+        for (int i = 1; i < n; i++) {
+            while (j > 0 && pattern.charAt(i) != pattern.charAt(j)) {
+                j = prefixArray[j - 1];
             }
-            System.out.println();
 
+            if (pattern.charAt(i) == pattern.charAt(j)) {
+                j++;
+            }
+
+            prefixArray[i] = j;
         }
 
-        System.out.println("Reciever recieved all the Frames");
-        System.out.println("Recieved Frames are");
-        System.out.println(arr);
+        return prefixArray;
+    }
+
+    // Helper function to check if str1 is both a prefix and a suffix of str2 using KMP
+    static boolean isPrefixAndSuffix(String str1, String str2) {
+        String concatenated = str1 + "#" + str2; // Concatenate str1 and str2 with a separator #
+
+        int[] prefixArray = computePrefixArray(concatenated);
+        int n1 = str1.length();
+        int n2 = concatenated.length();
+
+        return prefixArray[n2 - 1] == n1;
+    }
+
+    // Main function to count the number of index pairs
+    static int countPrefixAndSuffixPairs(String[] words) {
+        int count = 0;
+        int n = words.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (isPrefixAndSuffix(words[i], words[j])) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
 
 
+        String[] words = {"a","ca"};
+
+        int result = countPrefixAndSuffixPairs(words);
+
+        System.out.println("Number of index pairs: " + result);
     }
 }
-
